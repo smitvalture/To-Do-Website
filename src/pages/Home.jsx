44887c5fb19ffb85677d7a302d8056559a3f3887
Smit Valture <valture16@gmail.com>
2023-06-30 00:59:04 +0530

    init

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import imageQR from '../assets/images/image-qr-code.png'
+
+const Home = () => {
+  return (
+    <section className='w-screen h-screen flex justify-center items-center bg-[#d6e0ed]'></section>
+  )
+}
+
+export default Home
